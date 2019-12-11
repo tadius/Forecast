a15da9d5dc4df5fa422d6d8cfcd367e8c5ab10a2
tadiuzzz <tadiuzzz@gmail.com>
2019-12-11 21:55:18 +0300

    Configured DI core. Added Network DI module. Added API constants.

---
+++ app/src/main/java/com/tadiuzzz/forecast/data/source/WeatherApi.kt
@@ -0,0 +1,7 @@
+package com.tadiuzzz.forecast.data.source
+
+interface WeatherApi {
+
+   //TODO: add api methods
+
+}
---
+++ app/src/main/java/com/tadiuzzz/forecast/di/module/NetworkModule.kt
@@ -0,0 +1,56 @@
+package com.tadiuzzz.newsapp.di.module
+
+import com.tadiuzzz.forecast.data.source.WeatherApi
+import com.tadiuzzz.newsapp.common.Constants.Companion.API_KEY
+import com.tadiuzzz.newsapp.common.Constants.Companion.BASE_URL
+import com.tadiuzzz.newsapp.di.scope.AppScope
+import dagger.Module
+import dagger.Provides
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import java.util.concurrent.TimeUnit
+
+@Module
+class NetworkModule {
+
+    @Provides
+    @AppScope
+    fun provideRetrofit(okHttpClient: OkHttpClient): Retrofit {
+        return Retrofit.Builder()
+            .baseUrl(BASE_URL)
+            .addConverterFactory(GsonConverterFactory.create())
+            .client(okHttpClient)
+            .build()
+    }
+
+    @Provides
+    @AppScope
+    fun provideOkHttpClient(): OkHttpClient {
+        val interceptor = HttpLoggingInterceptor()
+        interceptor.level = HttpLoggingInterceptor.Level.BODY
+        val builder = OkHttpClient.Builder()
+        builder.addInterceptor(interceptor)
+            .connectTimeout(30, TimeUnit.SECONDS)
+            .readTimeout(30, TimeUnit.SECONDS)
+            .addInterceptor { chain ->
+                val original = chain.request()
+                val originalUrl = original.url
+                val url = originalUrl.newBuilder()
+                    .addQueryParameter("APPID", API_KEY)
+                    .build()
+                val requestBuilder = original.newBuilder()
+                    .url(url)
+                val request = requestBuilder.build()
+                chain.proceed(request)
+            }
+        return builder.build()
+    }
+
+    @Provides
+    @AppScope
+    fun provideWeatherApi(retrofit: Retrofit): WeatherApi {
+        return retrofit.create(WeatherApi::class.java)
+    }
+}
