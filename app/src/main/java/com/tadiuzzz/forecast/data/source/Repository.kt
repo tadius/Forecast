5ccbe6218e4d20059c26776c2e0a1c6a122336fe
tadiuzzz <tadiuzzz@gmail.com>
2019-12-11 22:46:08 +0300

    Added methods fot getting current weather.

--- app/src/main/java/com/tadiuzzz/forecast/data/source/Repository.kt
+++ app/src/main/java/com/tadiuzzz/forecast/data/source/Repository.kt
@@ -1,7 +1,12 @@
 package com.tadiuzzz.forecast.data.source
 
-import com.tadiuzzz.forecast.data.source.WeatherApi
+import com.tadiuzzz.forecast.data.WeatherResponse
+import retrofit2.Response
 import javax.inject.Inject
 
 class Repository @Inject constructor(val weatherApi: WeatherApi) {
+
+    suspend fun getCurrentWeather(city: String): Response<WeatherResponse> {
+        return weatherApi.getCurrentWeather(city)
+    }
 }
--- app/src/main/java/com/tadiuzzz/forecast/data/source/WeatherApi.kt
+++ app/src/main/java/com/tadiuzzz/forecast/data/source/WeatherApi.kt
@@ -1,7 +1,12 @@
 package com.tadiuzzz.forecast.data.source
 
+import com.tadiuzzz.forecast.data.WeatherResponse
+import retrofit2.Response
+import retrofit2.http.GET
+import retrofit2.http.Query
+
 interface WeatherApi {
 
-   //TODO: add api methods
-
+   @GET("weather")
+   suspend fun getCurrentWeather(@Query("q") cityName: String): Response<WeatherResponse>
 }
